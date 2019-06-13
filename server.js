33a367c18c68a7e225a3319f50072b313301420d
omgitsgod <omgitsgod@gmail.com>
2019-06-13 15:49:41 -0400

    Add cors to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const constants = require('./constants')
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ app.get('/', route);
 app.listen(port, function() {
   console.log('app is running  on port '+port);
 });
-app.get('/test/:x/:y', function(req, res) {
+app.get('/test/:x/:y', cors(), function(req, res) {
   console.log(req.params);
   res.json(constants.weather);
 });
