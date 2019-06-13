5dcbe2a1c2e0453645edce1cb67ed18ed4f93289
omgitsgod <omgitsgod@gmail.com>
2019-06-13 14:08:56 -0400

    Test route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-app.get('/', function(req, res) {
+app.get('/test', function(req, res) {
   res.send('Response recorded for Homepage!!');
 });
 app.listen(port, function() {
