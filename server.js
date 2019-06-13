4aac80e48588c11be92eade058ebed4a9e0a4bba
omgitsgod <omgitsgod@gmail.com>
2019-06-13 14:25:00 -0400

    Use route.js for routing

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const route = require('./route')
 
-app.get('/', function(req, res) {
-  res.send('Response recorded for Homepage!!');
-});
+app.get('/', route);
 app.listen(port, function() {
   console.log('app is running  on port '+port);
 });
