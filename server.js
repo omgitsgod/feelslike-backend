f015cb184b676f9bd65072b5fd9221942d847a2e
omgitsgod <omgitsgod@gmail.com>
2019-06-13 14:04:42 -0400

    Change entry and start command in script/Conditionally render port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-let app = express();
+const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', function(req, res) {
   res.send('Response recorded for Homepage!!');
 });
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log('app is running  on port 3000');
 });
