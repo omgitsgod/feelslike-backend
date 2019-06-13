f7f3a9def1049e5916900bd835cfa5eff6b41fa4
omgitsgod <omgitsgod@gmail.com>
2019-06-13 14:17:50 -0400

    Change route to /

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/test', function(req, res) {
+app.get('/', function(req, res) {
   res.send('Response recorded for Homepage!!');
 });
 app.listen(port, function() {
