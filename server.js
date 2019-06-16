3dd1acc273687d980f0b01382142d3fbfb013e57
omgitsgod <omgitsgod@gmail.com>
2019-06-16 13:34:56 -0400

    Test getting rid of braces on data

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ const api = process.env.WEATHERAPI
 const app = express();
 const port = process.env.PORT || 5000;
 const route = require('./route')
+app.use(cors())
 
 app.get('/', route);
 app.listen(port, function() {
   console.log('app is running  on port '+port);
   console.log(process.env.AHHH);
 });
-app.get('/test/:x/:y', cors(), function(req, res) {
+app.get('/test/:x/:y', function(req, res) {
   console.log(req.params);
   const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${api}/${req.params.x},${req.params.y}`
   fetch(url)
   .then(r => r.json())
-  .then(data => {console.log(data);res.send({data})})
+  .then(data => {console.log(data);res.send(data)})
 });
