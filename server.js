04734867d503edf24529c9bedc7852346d66f249
omgitsgod <omgitsgod@gmail.com>
2019-06-16 17:35:48 -0400

    Consolelog test2

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get('/test/:x/:y', cors(), function(req, res) {
   console.log(req.params);
   const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${api}/${req.params.x},${req.params.y}`
   fetch(url)
-  .then(r => r.json())
   .then(console.log)
+  //.then(console.log)
 });
