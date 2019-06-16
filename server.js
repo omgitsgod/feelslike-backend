c3a4d3bf61d94bcd729389593a9d7591232e6786
omgitsgod <omgitsgod@gmail.com>
2019-06-16 13:26:38 -0400

    Test with console log

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('/test/:x/:y', cors(), function(req, res) {
   console.log(req.params);
   const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${api}/${req.params.x},${req.params.y}`
   fetch(url)
-  .then(data => res.send({data}))
+  .then(data => {console.log(data);res.send({data})})
 });
