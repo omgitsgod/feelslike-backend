8fe7b54c19d55c28d1b72acf865a11ff1bad6dc2
omgitsgod <omgitsgod@gmail.com>
2019-06-19 13:04:01 -0400

    Get rid of tests

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
 require('dotenv').config();
-const constants = require('./constants')
 const cors = require('cors')
 const fetch   = require('node-fetch');
 const api = process.env.WEATHERAPI
 
 const app = express();
 const port = process.env.PORT || 5000;
-const route = require('./route')
 
-app.get('/', route);
-app.listen(port, function() {
-  console.log('app is running  on port '+port);
-  console.log(process.env.AHHH);
-});
-app.get('/test/:x/:y', cors(), function(req, res) {
+
+app.get('/get/:x/:y', cors(), function(req, res) {
   console.log(req.params);
-  //${req.params.x},${req.params.y}
   const url = `https://api.darksky.net/forecast/${api}/${req.params.x},${req.params.y}`
   fetch(url)
   .then(res => res.json())
