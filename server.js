ca5852ed2ca4f83ee259ebf49a7690cc9394a3aa
omgitsgod <omgitsgod@gmail.com>
2019-06-19 13:19:37 -0400

    Change to get

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log('app is up and running  on port '+port);
 });
-app.get('/test/:x/:y', cors(), function(req, res) {
+app.get('/get/:x/:y', cors(), function(req, res) {
   console.log(req.params);
   const url = `https://api.darksky.net/forecast/${api}/${req.params.x},${req.params.y}`
   fetch(url)
