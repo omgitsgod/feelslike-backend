494509279cca122cc4583f3e02e7851b0b27de74
omgitsgod <omgitsgod@gmail.com>
2019-06-19 13:15:30 -0400

    Fix listen error

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 require('dotenv').config();
-const cors = require('cors')
+const cors = require('cors');
 const fetch   = require('node-fetch');
 const api = process.env.WEATHERAPI
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-app.get('/get/:x/:y', cors(), function(req, res) {
+app.listen(port, function() {
+  console.log('app is up and running  on port '+port);
+});
+app.get('/test/:x/:y', cors(), function(req, res) {
   console.log(req.params);
   const url = `https://api.darksky.net/forecast/${api}/${req.params.x},${req.params.y}`
   fetch(url)
